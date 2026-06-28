681d4625327d21afcad7d9f5f27a340c36398db9
cocot-gf <gadget_factory@bf.wakwak.com>
2026-06-28 09:53:06 +0900

    Delete AibbyScopeDT/driver/clean.bat

--- AibbyScopeDT/driver/clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for %%a in (inc\*.*) do (echo %%a & copy /b /y nul %curdir%%%a)
-for %%a in (lib\*.*) do (echo %%a & copy /b /y nul %curdir%%%a)
-for %%a in (src\*.*) do (echo %%a & copy /b /y nul %curdir%%%a)
-dir inc lib src
-echo.
-echo Clean Finished.
-echo.
-pause
